84ce608238c11edc452e77ac68f403835f83b4ce
康智超 <kangzhichao@3dbox.com>
2024-09-06 10:47:51 +0800

    整理了payload发送和接收函数

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,23 @@
 #include "printf.h"
 #include "RF24.h"
 #include "printf.h"
+#include "NRF24_device.h"
+void TaskReceiveFromSend(void *pvParameters);
 
 // nRF24L01引脚配置
 #define CE_PIN 2
 #define CSN_PIN 3
-RF24 radio(CE_PIN, CSN_PIN);
+// RF24 radio(CE_PIN, CSN_PIN);
 
 // 接收地址
 uint8_t address[][6] = {"1Node", "2Node", "3Node"};
 
-struct PayloadStruct
-{
-  char message[12]; // 传输的消息
-  uint16_t counter; // 计数器
-};
-PayloadStruct payload;
+// struct PayloadStruct
+// {
+//   char message[12] = {"PayLoad ack"}; // 传输的消息
+//   uint16_t counter = 0;               // 计数器
+// };
+// PayloadStruct payload;
 
 // 记录上一次接收到数据的时间
 unsigned long last_received_time = 0;
@@ -38,61 +40,69 @@ void setup()
     } // 进入无限循环
   }
   radio.setCRCLength(RF24_CRC_8);
-  radio.setDataRate(RF24_2MBPS);
+  radio.setDataRate(RF24_1MBPS);
   radio.setRetries(2, 5);
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_HIGH);
   radio.enableDynamicPayloads();
   radio.enableAckPayload();
   radio.openWritingPipe(address[3]);    // 设置发送地址 (用于ACK)
   radio.openReadingPipe(1, address[1]); // 设置接收地址
-  radio.startListening();               // 设置为接收模式
 
   radio.printDetails();
   radio.setChannel(100);
+  radio.flush_tx();
 
-  payload.counter = 0;                                 // 初始化计数器
-  memcpy(payload.message, "RX World 0", 14);             // 设置接收消息
   radio.writeAckPayload(1, &payload, sizeof(payload)); // 预载入ACK负载
-
+  radio.startListening();                              // 设置为接收模式
+  ackPayload.counter = 0;
+  // xTaskCreate(
+  //     TaskReceiveFromSend, // 任务函数
+  //     "ReceiveFromSend",   // 任务名称
+  //     2048,                // 堆栈大小（字节）
+  //     NULL,                // 任务参数
+  //     1,                   // 任务优先级
+  //     NULL                 // 任务句柄
+  // );
   // 初始化上一次接收时间
   last_received_time = millis();
 }
 
 void loop()
 {
-  uint8_t pipe;
-  if (radio.available(&pipe))
-  {                                                             // 检查是否有数据到达
-    unsigned long current_time = millis();                      // 当前时间
-    unsigned long interval = current_time - last_received_time; // 计算间隔时间
-    last_received_time = current_time;                          // 更新上一次接收时间
+  memcpy(&ackPayload, "payload ack", sizeof("payload ack"));
+  // 调用函数
+  ReceivedData receivedData = handleRadioReceive(&ackPayload, sizeof(ackPayload));
+}
+void TaskReceiveFromSend(void *pvParameters)
+{
+  while (1)
+  {
+    uint8_t pipe;
+    if (radio.available(&pipe))
+    { // is there a payload?
+      auto payloadsize = radio.getDynamicPayloadSize();
+      uint8_t received[payloadsize] = {0};
+      radio.read(&received, sizeof(received)); // get incoming payload
+      Serial.print("Received ");
+      Serial.print(payloadsize);
+      Serial.print(" bytes on pipe ");
+      Serial.print(pipe);
+      Serial.print(": ");
+      for (int i = 0; i < payloadsize; i++)
+      {
+        Serial.print("0x");
+        Serial.print((received[i]));
+        Serial.print(" ");
+      }
 
-    uint8_t bytes = radio.getDynamicPayloadSize(); // 获取负载大小
-    // PayloadStruct received;
-    uint8_t received[bytes] = {0};
-    radio.read(received, bytes); // 读取数据
-    Serial.print(F("Received "));
-    Serial.print(bytes); // 打印负载大小
-    Serial.print(F(" bytes on pipe "));
-    Serial.print(pipe); // 打印管道编号
-    Serial.print(F(": "));
-    // Serial.print(received.message); // 打印收到的消息
-    // Serial.print(received.counter); // 打印收到的计数器
-    // Serial.print(F(" Sent: "));
-    // Serial.print(payload.message); // 打印发送的消息
-    // Serial.print(payload.counter); // 打印发送的计数器
-    // 打印接收到的数据
-    for (int i = 0; i < bytes; i++)
-    {
-      Serial.print("0x");
-      Serial.print((received[i]));
-      Serial.print(" ");
+      // transmit response & save result to `report`
+      radio.stopListening(); // put in TX mode
+
+      radio.writeFast(&payload, sizeof(payload), false); // load response to TX FIFO
+      // radio.txStandBy(150);                       // keep retrying for 150 ms
+
+      radio.startListening(); // put back in RX mode
     }
-    Serial.print(F(" Interval: "));
-    Serial.print(interval); // 打印时间间隔
-    Serial.println(F(" ms"));
-    // payload.counter = received.counter + 1;              // 更新计数器
-    payload.counter = received[0]+1;
-    radio.writeAckPayload(1, &payload, sizeof(payload)); // 预载入新的ACK负载
+    vTaskDelay(10);
   }
 }
